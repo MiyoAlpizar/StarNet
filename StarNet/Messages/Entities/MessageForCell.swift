f6211fae6bdd0002d299116023266cbb98d29571
Miyo Alpizar <miyo.naval@gmail.com>
2022-01-08 19:30:23 -0600

    Messages Module created

---
+++ StarNet/Messages/Entities/MessageForCell.swift
@@ -0,0 +1,16 @@
+//
+//  MessageForCell.swift
+//  StarNet
+//
+//  Created by Miyo on 08/01/22.
+//
+
+import Foundation
+
+struct MessageForCell {
+    var imageURL: URL?
+    let userName: String
+    let preview: String
+    let newMessages: Int
+    var timeAgo: String
+}
